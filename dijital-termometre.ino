9ae3196475535d8c89ab0c2d3153cfb7704153c1
Ahmet Emin Yakar <ahmeteminyakar@gmail.com>
2025-03-25 02:44:33 +0300

    Update dijital-termometre.ino

--- dijital-termometre.ino
+++ dijital-termometre.ino
@@ -1,31 +1,53 @@
-#include <DHT.h>
+#include <LiquidCrystal_I2C.h>
 
-#define DHTPIN 2         // DHT11 sensörü Arduino'nun 2 numaralı pinine bağlı
-#define DHTTYPE DHT11    // Kullanılan sensör tipi
+// LM35 sensör A0 pininde
+const int lm35Pin = A0;
 
-DHT dht(DHTPIN, DHTTYPE);  // Sensör nesnesi oluşturuluyor
+// LCD adres ve boyut (I2C)
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// Min/Maks sıcaklık takibi
+float minTemp = 100.0;
+float maxTemp = -100.0;
+
+// Uyarı eşiği
+const float tempThreshold = 30.0;
 
 void setup() {
-  Serial.begin(9600);    // Seri haberleşme başlatılıyor
-  dht.begin();           // Sensör başlatılıyor
+  lcd.begin();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Termometre Basladi");
+  delay(2000);
+  lcd.clear();
 }
 
 void loop() {
-  float temp = dht.readTemperature();     // Sıcaklık verisi
-  float hum = dht.readHumidity();         // Nem verisi
+  int analogValue = analogRead(lm35Pin);
+  float voltage = analogValue * (5.0 / 1023.0);
+  float temperatureC = voltage * 100;
 
-  // Sensör okuması başarısızsa
-  if (isnan(temp) || isnan(hum)) {
-    Serial.println("Sensor okunamadi!");
-    return;
+  // Min / Maks güncelle
+  if (temperatureC < minTemp) minTemp = temperatureC;
+  if (temperatureC > maxTemp) maxTemp = temperatureC;
+
+  // LCD yazdir
+  lcd.setCursor(0, 0);
+  lcd.print("Anlik: ");
+  lcd.print(temperatureC, 1); // 1 basamakli
+  lcd.print((char)223); // ° simgesi
+  lcd.print("C ");
+
+  if (temperatureC > tempThreshold) {
+    lcd.setCursor(0, 1);
+    lcd.print("YUKSEK SICAKLIK!");
+  } else {
+    lcd.setCursor(0, 1);
+    lcd.print("Min:");
+    lcd.print(minTemp, 0);
+    lcd.print(" Max:");
+    lcd.print(maxTemp, 0);
   }
 
-  // Verileri seri monitörde göster
-  Serial.print("Sicaklik: ");
-  Serial.print(temp);
-  Serial.print(" °C, Nem: ");
-  Serial.print(hum);
-  Serial.println(" %");
-
-  delay(2000); // 2 saniyede bir ölçüm yapılır
+  delay(2000);
 }
